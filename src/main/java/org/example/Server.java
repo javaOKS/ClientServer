3793337a915f69a64d82c0a3e97eaeb726a1eec5
Pilkin <okspilkin@gmail.com>
2023-03-01 17:01:01 +0200

    Homework9

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,6 +3,11 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+;
 
 public class Server {
     public static void main(String[] args) {
@@ -11,16 +16,34 @@ public class Server {
             System.out.println("Waiting for a client...");
             try (Socket socket = serverSocket.accept();
 
-                 BufferedReader serverInput = new BufferedReader(new InputStreamReader(System.in));
+
                  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                  PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
                 String strClient;
+                String dateTime = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm:ss a")
+                        .format(LocalDateTime.now());
+                Pattern pattern = Pattern.compile("[эъы]");
+                Matcher m;
+                String parting = "BAY!";
                 String strServer = "Привіт!";
                 out.println(strServer);
-                while (!(strClient = in.readLine()).equals("вихід")) {
-                    System.out.println("Client says :" + strClient);
-                    strServer = serverInput.readLine();
-                    out.println("Server says: " + strServer);
+                while (!(strServer.equals(parting))) {
+                    strClient = in.readLine();
+                    System.out.println("Client says: " + strClient);
+                    if ((m = pattern.matcher(strClient)).find()) {
+                        out.println("Що таке паляниця?");
+                        strClient = in.readLine();
+                        if (strClient.equals("хліб")) {
+                            strServer = parting;
+                            out.println(dateTime + " " + strServer);
+                        } else {
+                            strServer = parting;
+                            out.println(strServer);
+                        }
+                    } else {
+                        strServer = parting;
+                        out.println(dateTime + "  " + strServer);
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
